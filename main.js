da3dfacab2977cbd3073195db22e2d804b74dac6
sanjar6767 <rasulberdievsanjar@gmail.com>
2025-10-21 18:30:44 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+
+let ism = prompt("isminggizni kiriting", "sanjar");
+console.log(ism)
+
+let fa = prompt("familiyani kiriting");
+console.log(fa)
+
+let yosh = prompt("yoshingiz kiriting");
+console.log(yosh)
+
+let yonalish = prompt("yonalish" , "IT");
+console.log(yonalish)
+
+let manzil=prompt("manzil");
+console.log(manzil);
+
+alert("malumotlar uchun rahmat")
